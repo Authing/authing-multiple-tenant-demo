928e58202e49270b9dff901782d5a459288e874b
张开宇 <zhangkaiyu@authing.cn>
2022-12-14 15:39:20 +0800

    add authing apis

--- server/app.js
+++ server/app.js
@@ -1,54 +1,19 @@
 const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const authMiddleware = require("./middlewares/auth.middleware");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
-const { sequelize } = require("./config/getConnection");
-
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const config = require("./config");
 
 const app = express();
 
 app.use(cors()); // 允许跨域
 
-// 测试数据库连接
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log(">>>>>>>>>>数据库连接成功<<<<<<<<<<");
-  })
-  .catch(() => console.log(">>>>>>>数据库连接失败，请重试<<<<<<<<<"));
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/*", authMiddleware);
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use(
-  "/authing-tenant",
-  createProxyMiddleware({
-    target: config.authing.host,
-    pathRewrite(path, req) {
-      console.log(path);
-      return path;
-    },
-  })
-);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- server/config.js
+++ server/config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  authing: {
-    // 用户池 ID
-    userPoolId: 'xxxxxxxxxxxxxxxxx',
-    // 用户池秘钥
-    userPoolSecret: 'xxxxxxxxxxxxxxxxx',
-    // 应用 ID
-    appId: 'xxxxxxxxxxxxxxxxx',
-    // 应用密钥
-    appSecret: 'xxxxxxxxxxxxxxxxx',
-    // Authing api 服务地址，若非私有部署为 https://core.authing.cn
-    host: 'https://core.authing.cn',
-  },
-}
+  // Authing api 服务地址，若非私有部署为 https://core.authing.cn
+  // host: "https://core.authing.cn",
+  host: "http://console.authing.localhost",
+};
