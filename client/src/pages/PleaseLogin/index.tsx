5dad74461ee1e5f574d7bd36fe45058315568d3f
Danker <hfull0578@163.com>
2022-12-27 17:44:24 +0800

    feat(PleaseLogin): 添加主页

---
+++ client/src/pages/PleaseLogin/index.tsx
@@ -0,0 +1,28 @@
+import "./index.less";
+import "@authing/react18-ui-components/lib/index.min.css";
+
+import TenantDefaultDoor from "@/assets/tenant-default-door.svg";
+import { Button } from "antd";
+import { useCallback } from "react";
+import { BASE_URL } from "@/utils/baseUrl";
+
+export const PleaseLogin = () => {
+  //TODO: 判断是否登录，有登录态则重定向后续页面
+  const handleClick = useCallback(() => {
+    window.location.href = `${BASE_URL}/login`;
+  }, []);
+  return (
+    <div className="authing-mtd-please-login">
+      <img src={TenantDefaultDoor} alt="" />
+      <h3>请先注册 / 登录，开启 Authing 多租户之旅</h3>
+      <Button
+        className="btn-registry-login"
+        type="primary"
+        size="large"
+        onClick={handleClick}
+      >
+        注册/登录
+      </Button>
+    </div>
+  );
+};
