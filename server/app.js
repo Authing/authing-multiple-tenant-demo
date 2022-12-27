6652e2349812cbad9688dd38be96e27b24615c84
张开宇 <zhangkaiyu@authing.cn>
2022-12-27 17:10:37 +0800

    single file

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const createError = require("http-errors");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const debug = require("debug")("server:server");
+const http = require("http");
 
 const indexRouter = require("./routes/index");
 
@@ -31,4 +33,55 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = Number(process.env.PORT) || 3000;
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`HTTP 服务已启动，监听端口: ${port}，进程 ID：${process.pid}`);
+});
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
