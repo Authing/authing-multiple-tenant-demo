dbf9980c29e1a7dae3ca9646efee5fbb339efd70
Danker <hfull0578@163.com>
2022-12-28 14:37:25 +0800

    feat(DocExplain): 添加文档阐述

---
+++ client/src/pages/DocExplain/index.tsx
@@ -0,0 +1,18 @@
+import "./index.less";
+
+export const DocExplain = () => {
+  return (
+    <div className="authing-mtd-doc-explain">
+      <h4 className="mtd-title">创建租户</h4>
+      <h4 className="mtd-title">修改租户信息</h4>
+      <h4 className="mtd-title">创建租户成员</h4>
+      <h4 className="mtd-title">修改租户成员信息</h4>
+      <h4 className="mtd-title">
+        更多 API 可查看&nbsp;
+        <a href="https://api.authing.cn/openapi/v3/management/">
+          https://api.authing.cn/openapi/v3/management/
+        </a>
+      </h4>
+    </div>
+  );
+};
