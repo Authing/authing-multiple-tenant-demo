c4cce44636d742160d42e67f6bd17b9a3faf8718
Danker <hfull0578@163.com>
2022-12-29 09:41:31 +0800

    feat(header): 完善 Header

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -1,7 +1,27 @@
-import { Layout } from "antd";
+import env from "@/config/env";
+import { ToolOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Button, ButtonProps, Layout } from "antd";
+import "./index.less";
 
 export interface HeaderProps {}
 
 export default (props: HeaderProps) => {
-  return <Layout.Header>hahhah</Layout.Header>;
+  const buttons: ButtonProps[] = [
+    {
+      icon: <ToolOutlined />,
+      children: "API",
+      href: env("DOC_API_URL"),
+      target: "_blank",
+    },
+  ];
+  return (
+    <Layout.Header className="mtd-header">
+      <div className="mtd-header-btns">
+        {buttons.map((it) => (
+          <Button type="link" {...it} />
+        ))}
+      </div>
+      <Avatar className="mtd-avatar" icon={<UserOutlined />} />
+    </Layout.Header>
+  );
 };
