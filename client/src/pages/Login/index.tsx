76ab8b245b52c5b5bcae609ef5263fdca93efa89
Danker <hfull0578@163.com>
2023-01-10 17:13:06 +0800

    feat(login): 添加授权登录

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,21 +1,44 @@
 import "./index.less";
 
+import { Button, Spin } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import { Navigate, useSearchParams } from "react-router-dom";
+
+import { getTokenByCode } from "@/api/auth";
 import TenantDefaultDoor from "@/assets/tenant-default-door.svg";
-import { Button } from "antd";
-import { useCallback, useEffect } from "react";
 import { BASE_URL } from "@/utils/baseUrl";
-import { useNavigate } from "react-router-dom";
-import { StepConfig, STEPS } from "../StepHome/stepConfig";
+import { checkAuth } from "@/utils/checkToken";
 
 export const Login = () => {
-  const nav = useNavigate();
-  //TODO: 判断是否登录，有登录态则重定向后续页面
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+  const [ready, setReady] = useState(false);
+  const [isPass, setIsPass] = useState(false);
+
+  useEffect(() => {
+    checkAuth()
+      .then(() => setIsPass(true))
+      .catch(() => {
+        if (!code) {
+          setIsPass(false);
+          return;
+        }
+        return getTokenByCode({ code })
+          .then(({ data }) => {
+            console.log(data);
+          })
+          .catch(() => {
+            setIsPass(false);
+          });
+      })
+      .finally(() => setReady(true));
+  }, [code]);
   const handleClick = useCallback(() => {
     window.location.href = `${BASE_URL}/login`;
-    // nav(`/step/${StepConfig[STEPS.step1].path}`);
   }, []);
-  useEffect(() => {}, []);
-  return (
+
+  if (ready && isPass) return <Navigate to="/step" />;
+  return ready ? (
     <div className="authing_mtd-please-login">
       <img src={TenantDefaultDoor} alt="" />
       <h3>请先注册 / 登录，开启 Authing 多租户之旅</h3>
@@ -28,5 +51,16 @@ export const Login = () => {
         注册/登录
       </Button>
     </div>
+  ) : (
+    <Spin
+      size="large"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    />
   );
 };
