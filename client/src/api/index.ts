86d7444437b50dfb0706c2e173a173f716907af4
Danker <hfull0578@163.com>
2023-01-11 15:47:24 +0800

    feat(api): 添加注释

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig } from "@/interface";
 import axios from "axios";
 
-export const getPublicConfig = (appId: string) => {
+/** 获取应用配置 */
+export const getApplicationConfig = (appId: string) => {
   return axios.get(`/applications/config/${appId}`) as Promise<
     AuthingResponse<ApplicationConfig>
   >;
