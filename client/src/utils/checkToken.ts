c566a9499778b77fc023d7d112cc8f22b750501e
Danker <hfull0578@163.com>
2023-01-12 19:06:42 +0800

    feat(utils): 优化

--- client/src/utils/checkToken.ts
+++ client/src/utils/checkToken.ts
@@ -1,9 +1,7 @@
-import env from "@/config/env";
 import { getToken, removeToken } from "./tokenStore";
 
 /** 检查 token 是否失效 */
 export const checkAuth = () => {
-  const key = env("TOKEN_KEY") ?? "";
   try {
     const token = getToken();
     const payload = JSON.parse(window.atob(token?.split(".")?.[1] ?? ""));
