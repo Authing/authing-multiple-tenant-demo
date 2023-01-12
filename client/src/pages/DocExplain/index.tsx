08a57053e1834e4c4075c9633c10eac450fd2acd
Danker <hfull0578@163.com>
2023-01-12 13:44:45 +0800

    feat(pages): 添加文档阐述

--- client/src/pages/DocExplain/index.tsx
+++ client/src/pages/DocExplain/index.tsx
@@ -1,18 +1,13 @@
 import "./index.less";
 
-import env from "@/config/env";
+import { Markdown } from "@/components/Markdown";
+
+import markdownText from "./explain.md?raw";
 
 export const DocExplain = () => {
   return (
     <div className="authing_mtd-doc-explain">
-      <h4 className="mtd-title">创建租户</h4>
-      <h4 className="mtd-title">修改租户信息</h4>
-      <h4 className="mtd-title">创建租户成员</h4>
-      <h4 className="mtd-title">修改租户成员信息</h4>
-      <h4 className="mtd-title">
-        更多 API 可查看&nbsp;
-        <a href={env("DOC_API_URL")}>{env("DOC_API_URL")}</a>
-      </h4>
+      <Markdown markdown={markdownText} />
     </div>
   );
 };
